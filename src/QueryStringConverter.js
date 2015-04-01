8316de8eff6fd25c36224d3bfe3cf69845a691fb
Dave <david.losert@gmx.de>
2015-04-01 19:19:16 +0200

    Added "silentErrors" option.

--- src/QueryStringConverter.js
+++ src/QueryStringConverter.js
@@ -4,37 +4,47 @@
  */
 
 const _ = require('lodash'),
-    queryString = require('querystring'),
-    qsErrors = require('./errors');
+  queryString = require('querystring'),
+  qsErrors = require('./errors');
 
 var QueryStringConverter = function (options) {
-    if (!options) {
-        throw new Error('Missing Options Object');
+  if (!options) {
+    throw new Error('Missing Options Object');
+  }
+  else if (!options.adapter) {
+    throw new qsErrors.MissingAdapter('No Adapter defined. Define it within the Options-Object.');
+  }
+
+  var adapter = options.adapter;
+
+  this.convertQuery = function (query) {
+    var parsedQuery = queryString.parse(query);
+    var result = {};
+
+    _.each(parsedQuery, function (queryElementValue, queryElementKey) {
+      let adapterElement = adapter.get(queryElementKey);
+      try {
+        validateQueryElement(adapterElement, queryElementKey, queryElementValue);
+        result[adapterElement.key] = adapterElement.convertQueryValue(queryElementValue);
+      } catch (error) {
+        if (!options.silentErrors) {
+          throw error;
+        }
+      }
+    });
+
+    return result;
+  };
+
+  function validateQueryElement(adapterElement, queryElementKey, queryElementValue) {
+    if (!adapterElement) {
+      throw new qsErrors.InvalidQueryParameter('Invalid query parameter with key "' + queryElementKey + '"!');
+    } else if (!queryElementValue.match(adapterElement.validate)) {
+      throw new qsErrors.InvalidQueryValue('Validation failed for Query-Value "' + queryElementValue + '"!');
     }
-    else if (!options.adapter) {
-        throw new qsErrors.MissingAdapter('No Adapter defined. Define it within the Options-Object.');
-    }
-
-    var adapter = options.adapter;
-
-    this.convertQuery = function (query) {
-        var parsedQuery = queryString.parse(query);
-        var result = {};
-
-        _.each(parsedQuery, function (value, key) {
-            let adapterElement = adapter.get(key);
-            if (!adapterElement && !options.silentErrors) {
-                throw new qsErrors.InvalidQueryParameter('Invalid query parameter with key "' + key + '"!');
-            } else if(!value.match(adapterElement.validate)){
-                throw new qsErrors.InvalidQueryValue('Validation failed for Query-Value "' + value + '"!');
-            }
-
-            result[adapterElement.key] = adapterElement.convertQueryValue(value);
-        });
-
-        return result;
-    };
+  }
 };
 
 
 module.exports = QueryStringConverter;
+
--- tests/unit/QueryStringConverter.test.js
+++ tests/unit/QueryStringConverter.test.js
@@ -5,111 +5,140 @@
 
 
 const chai = require("chai"),
-	sinon = require("sinon"),
-	sinonChai = require("sinon-chai"),
-	expect = chai.expect,
-	_ = require('lodash');
+  sinon = require("sinon"),
+  sinonChai = require("sinon-chai"),
+  expect = chai.expect,
+  _ = require('lodash');
 
 chai.use(sinonChai);
 
 const QueryStringConverter = require('../../src/QueryStringConverter'),
-	errors = require('../../src/errors');
+  errors = require('../../src/errors');
 
 describe('QueryStringConverter', function () {
 
-	describe('#instance', function () {
+  describe('#instance', function () {
+    it('should throw error if no options object given', function () {
+      var instanceWrapper = function () {
+        new QueryStringConverter();
+      };
+      expect(instanceWrapper).to.throw('Missing Options Object');
+    });
 
-		it('should throw error if no options object given', function () {
-			var instanceWrapper = function () {
-				new QueryStringConverter();
-			};
-			expect(instanceWrapper).to.throw('Missing Options Object');
-		});
+    it('should throw if no adapter in options', function () {
+      var instanceWrapper = function () {
+        new QueryStringConverter({});
+      };
+      var errorMessage = 'No Adapter defined. Define it within the Options-Object.';
+      expect(instanceWrapper).to.throw(errors.MissingAdapter, errorMessage);
+    });
+  });
 
-		it('should throw if no adapter in options', function () {
-			var instanceWrapper = function () {
-				new QueryStringConverter({});
-			};
-			var errorMessage = 'No Adapter defined. Define it within the Options-Object.';
-			expect(instanceWrapper).to.throw(errors.MissingAdapter, errorMessage);
-		});
-	});
+  describe('#convertQuery()', function () {
+    var convertQuery,
+      qsConverterInstance,
+      testAdapter;
 
-	describe('#convertQuery()', function () {
-		var convertQuery,
-			qsConverterInstance,
-			testAdapter;
+    beforeEach(function () {
+      testAdapter = new Map();
+      qsConverterInstance = new QueryStringConverter({
+        adapter : testAdapter
+      });
+      convertQuery = qsConverterInstance.convertQuery;
+    });
 
-		beforeEach(function () {
-			testAdapter = new Map();
-			qsConverterInstance = new QueryStringConverter({
-				adapter : testAdapter
-			});
-			convertQuery = qsConverterInstance.convertQuery;
-		});
+    it('should exist', function () {
+      expect(qsConverterInstance.convertQuery).to.exist;
+    });
 
-		it('should exist', function () {
-			expect(convertQuery).to.exist;
-		});
+    it('should be a function', function () {
+      expect(qsConverterInstance.convertQuery).to.be.a('function');
+    });
 
-		it('should be a function', function () {
-			expect(convertQuery).to.be.a('function');
-		});
+    describe('adapter values', function () {
+      let testQuery;
+      let adapterElementMock;
 
-		describe('adapter values', function () {
-			let testQuery;
-			let adapterElementMock;
+      beforeEach(function () {
+        testQuery = 'limitTo=42';
+        adapterElementMock = {
+          key               : 'limit',
+          convertQueryValue : sinon.stub().returns(42)
+        };
+        testAdapter.set('limitTo', adapterElementMock);
+      });
 
-			beforeEach(function () {
-				testQuery = 'limitTo=42';
-				adapterElementMock = {
-					key               : 'limit',
-					convertQueryValue : sinon.stub().returns(42)
-				};
-				testAdapter.set('limitTo', adapterElementMock);
-			});
+      it('should call the #convertQueryValue-Method of a matching adapter-element with query value as argument', function () {
+        qsConverterInstance.convertQuery(testQuery);
+        expect(adapterElementMock.convertQueryValue).to.have.been.calledWith('42');
+      });
 
-			it('should call the #convertQueryValue-Method of a matching adapter-element with query value as argument', function () {
-				convertQuery(testQuery);
-				expect(adapterElementMock.convertQueryValue).to.have.been.calledWith('42');
-			});
+      it('should combine key and result of #convertQueryValue to result-object', function () {
+        let expectedResult = {'limit' : 42};
+        let result = qsConverterInstance.convertQuery(testQuery);
+        expect(result).to.deep.equal(expectedResult);
+      });
+    });
 
-			it('should combine key and result of #convertQueryValue to result-object', function () {
-				let expectedResult = {'limit' : 42};
-				var result = convertQuery(testQuery);
-				expect(result).to.deep.equal(expectedResult);
-			});
-		});
+    describe('validation', function () {
+      let adapterElementMock;
+      beforeEach(function () {
+        adapterElementMock = {
+          key               : 'test',
+          convertQueryValue : sinon.stub().returns(42),
+          validate          : /^a$/
+        };
+        testAdapter.set('testKey', adapterElementMock);
+      });
 
-        describe('validation', function () {
-            let adapterElementMock;
-            beforeEach(function () {
-                adapterElementMock = {
-                    key : 'test',
-                    convertQueryValue: sinon.stub().returns(42),
-                    validate: /^a$/
-                };
-                testAdapter.set('testKey', adapterElementMock);
-            });
+      it('should throw "InvalidQueryValue" if value doesnt match regex', function () {
+        var funcWrapper = function () {
+          qsConverterInstance.convertQuery('testKey=b')
+        };
+        expect(funcWrapper).to.throw(errors.InvalidQueryValue, 'Validation failed for Query-Value "b"!');
+      });
 
-            it('should throw "InvalidQueryValue" if value doesnt match regex', function () {
-                var funcWrapper = function(){convertQuery('testKey=b')};
-                expect(funcWrapper).to.throw(errors.InvalidQueryValue, 'Validation failed for Query-Value "b"!');
-            });
+      it('should throw "InvalidQueryParameter" on invalid query Parameter', function () {
+        let invalidQuery = 'invalidKey=invalidValue';
+        let expectedError = errors.InvalidQueryParameter;
+        expect(qsConverterInstance.convertQuery.bind(qsConverterInstance, invalidQuery)).to.throw(expectedError);
+      });
+    });
 
+    describe('silentErrors=true', function () {
+      let adapterElementMock;
+      beforeEach(function () {
+        adapterElementMock = {
+          key               : 'test',
+          convertQueryValue : sinon.stub().returns(42),
+          validate          : /^a$/
+        };
+        testAdapter.set('testKey', adapterElementMock);
+        qsConverterInstance = new QueryStringConverter({
+          adapter      : testAdapter,
+          silentErrors : true
         });
 
-		describe('special cases', function () {
-			it('should throw "InvalidQueryParameter" on invalid query Parameter', function () {
-				var invalidQuery = 'invalidKey=invalidValue';
-				var expectedError = errors.InvalidQueryParameter;
-				expect(convertQuery.bind(qsConverterInstance, invalidQuery)).to.throw(expectedError);
-			});
-		});
-	});
+      });
+      it('should not throw "InvalidQueryValue"', function () {
+        let invalidQuery = 'testKey=invalidValue';
+        var funcWrapper = function () {
+          qsConverterInstance.convertQuery(invalidQuery)
+        };
+        expect(funcWrapper).not.to.throw();
+      });
 
-//	fields  : 'attributes', // --> Comma separated like fields=name,id,createdAt
-//	where   : 'where', // --> Comma separated field:values pairs like field1:value1,field2:value2 (flat) - maybe
-// allow keywords? include : 'include' // --> model1(fieldName1,fieldName2),model2
+      it('should not throw "InvalidQueryParemeter"', function () {
+        let invalidQuery = 'invalidKey=invalidValue';
+        var funcWrapper = function () {
+          qsConverterInstance.convertQuery(invalidQuery)
+        };
+        expect(funcWrapper).not.to.throw();
+      });
 
+      it('should still convert valid queries', function () {
+
+      });
+    });
+  });
 });
